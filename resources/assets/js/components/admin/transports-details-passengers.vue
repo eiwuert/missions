bd73d2936516d241674569ea895cd404b21210e8
Jerez Bain <jerezb31@gmail.com>
2017-06-21 11:32:14 -0400

    closes #593

--- resources/assets/js/components/admin/transports-details-passengers.vue
+++ resources/assets/js/components/admin/transports-details-passengers.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<aside :show.sync="showPassengersFilters" placement="left" header="Passengers Filters" :width="375">
-			<reservations-filters v-ref:filters :filters.sync="passengersFilters" :reset-callback="resetPassengerFilters" :pagination="passengersPagination" :callback="getPassengers" storage="" teams></reservations-filters>
+			<!--<reservations-filters v-ref:filters :filters.sync="passengersFilters" :reset-callback="resetPassengerFilters" :pagination="passengersPagination" :callback="getPassengers" storage="" teams></reservations-filters>-->
 		</aside>
 		<aside :show.sync="showReservationsFilters" placement="left" header="Reservations Filters" :width="375">
 			<reservations-filters v-ref:filters :filters.sync="reservationFilters" :reset-callback="resetReservationFilters" :pagination="reservationsPagination" :callback="searchReservations" storage="" teams></reservations-filters>
@@ -11,12 +11,15 @@
 			<!-- Passengers List -->
 			<div class="col-sm-8">
 				<form class="form-inline row">
-					<div class="input-group input-group-sm col-sm-7">
-						<input type="text" class="form-control" v-model="passengersFilters.search" debounce="300" placeholder="Search Passengers">
-						<span class="input-group-addon"><i class="fa fa-search"></i></span>
+					<div class="col-sm-8">
+						<div class="input-group input-group-sm col-xs-12">
+							<input type="text" class="form-control" v-model="passengersFilters.search" debounce="300" placeholder="Search Passengers">
+							<span class="input-group-addon"><i class="fa fa-search"></i></span>
+						</div>
 					</div>
-					<div class="form-group col-sm-5">
-						<button class="btn btn-default btn-sm" @click="showPassengersFilters = true;">Filters</button>
+
+					<div class="form-group col-sm-4">
+						<a class="btn btn-default btn-sm btn-block" @click="showPassengersFilters = true;">Filters</a>
 					</div>
 					<div class="col-xs-12">
 						<hr class="divider inv">
@@ -65,21 +68,21 @@
 									<div class="col-xs-9">
 										<div class="media">
 											<div class="media-left" style="padding-right:0;">
-												<a :href="'/admin/reservations/' + passenger.reservation.id" target="_blank">
-													<img :src="passenger.reservation.avatar" class="img-circle img-xs av-left" style="margin-right: 10px">
+												<a :href="'/admin/reservations/' + passenger.reservation.data.id" target="_blank">
+													<img :src="passenger.reservation.data.avatar" class="img-circle img-xs av-left" style="margin-right: 10px">
 												</a>
 											</div>
 											<div class="media-body" style="vertical-align:middle;">
 												<h6 class="media-heading text-capitalize" style="margin-bottom:3px;">
 													<i :class="getGenderStatusIcon(passenger)"></i>
-													<a :href="'/admin/reservations/' + reservation.id" target="_blank">{{ passenger.reservation.surname | capitalize }}, {{ passenger.reservation.given_names | capitalize }}</a></h6>
-												<p style="line-height:1;font-size:10px;margin-bottom:2px;">{{ passenger.reservation.desired_role.name }} <span class="text-muted">&middot; {{ passenger.reservation.travel_group}}</span></p>
+													<a :href="'/admin/reservations/' + passenger.reservation.data.id" target="_blank">{{ passenger.reservation.data.surname | capitalize }}, {{ passenger.reservation.data.given_names | capitalize }}</a></h6>
+												<p style="line-height:1;font-size:10px;margin-bottom:2px;">{{ passenger.reservation.data.desired_role.name }} <span class="text-muted">&middot; {{ passenger.reservation.data.travel_group}}</span></p>
 											</div><!-- end media-body -->
 										</div><!-- end media -->
 									</div>
 									<div class="col-xs-3 text-right action-buttons">
 										<a class="btn btn-xs btn-primary-hollow" @click="removePassenger(passenger)">
-											<span class="fa fa-ellipsis-h"></span>
+											<span class="fa fa-minus"></span>
 										</a>
 										<a class="btn btn-xs btn-default-hollow" role="button" data-toggle="collapse" data-parent="#PassengerAccordion" :href="'#squadLeaderItem' + $index" aria-expanded="true" aria-controls="collapseOne">
 											<i class="fa fa-angle-down"></i>
@@ -93,21 +96,21 @@
 								<div class="row">
 									<div class="col-sm-6">
 										<label>Gender</label>
-										<p class="small">{{passenger.reservation.gender | capitalize}}</p>
+										<p class="small">{{passenger.reservation.data.gender | capitalize}}</p>
 										<label>Marital Status</label>
-										<p class="small">{{passenger.reservation.status | capitalize}}</p>
+										<p class="small">{{passenger.reservation.data.status | capitalize}}</p>
 									</div><!-- end col -->
 									<div class="col-sm-6">
 										<label>Age</label>
-										<p class="small">{{passenger.reservation.age}}</p>
+										<p class="small">{{passenger.reservation.data.age}}</p>
 										<label>Travel Group</label>
-										<p class="small">{{passenger.reservation.trip.data.group.data.name}}</p>
+										<p class="small">{{passenger.reservation.data.trip.data.group.data.name}}</p>
 									</div><!-- end col -->
 								</div><!-- end row -->
 								<div class="col-sm-12">
 									<label>Companions</label>
-									<ul class="list-unstyled" v-if="passenger.reservation.companions.data.length">
-										<li v-for="companion in passenger.reservation.companions.data">
+									<ul class="list-unstyled" v-if="passenger.reservation.data.companions.data.length">
+										<li v-for="companion in passenger.reservation.data.companions.data">
 											<i :class="getGenderStatusIcon(companion)"></i>
 											{{ companion.surname | capitalize }}, {{ companion.given_names | capitalize }}
 											<span class="text-muted">({{ companion.relationship | capitalize }})</span>
@@ -117,32 +120,37 @@
 								</div>
 								<div class="col-sm-6">
 									<label>Trip Type</label>
-									<p class="small">{{passenger.reservation.trip.data.type | capitalize}}</p>
+									<p class="small">{{passenger.reservation.data.trip.data.type | capitalize}}</p>
 								</div>
 								<div class="col-sm-6">
 									<label>Designation</label>
-									<p class="small">{{ passenger.reservation.arrival_designation }}</p>
+									<p class="small">{{ passenger.reservation.data.arrival_designation }}</p>
 								</div>
 							</div>
 						</div>
-						<div class="panel-footer small clearfix" style="background-color: #ffe000;" v-if="passenger.reservation.companions.data.length && companionsPresentSquad(passenger, squad)">
-							<i class=" fa fa-info-circle"></i> {{passenger.reservation.present_companions}} companions not in group &middot; {{companionsPresentTeam(passenger)}} not on this squad.
+						<div class="panel-footer small clearfix" style="background-color: #ffe000;" v-if="passenger.reservation.data.companions.data.length && companionsPresentSquad(passenger, squad)">
+							<i class=" fa fa-info-circle"></i> {{passenger.reservation.data.present_companions}} companions not in group &middot; {{companionsPresentTeam(passenger)}} not on this squad.
 							<button type="button" class="btn btn-xs btn-default-hollow pull-right" @click="addCompanionsToSquad(passenger, squad)"><i class="fa fa-plus-circle"></i> Companions</button>
 						</div>
 					</div>
+				</div>
+				<div class="col-sm-12 text-center">
+					<pagination :pagination.sync="passengersPagination" :callback="getPassengers"></pagination>
 				</div>
 
 			</div>
 			<!-- Reservations List -->
 			<div class="col-sm-4">
 				<form class="form-inline row">
-					<div class="input-group input-group-sm col-sm-7">
+					<div class="col-sm-8">
+					<div class="input-group input-group-sm col-xs-12">
 						<input type="text" class="form-control" v-model="reservationFilters.search" debounce="300" placeholder="Search Reservations">
 						<span class="input-group-addon"><i class="fa fa-search"></i></span>
 					</div>
+					</div>
 
-					<div class="form-group col-sm-5">
-						<button class="btn btn-default btn-sm" @click="showReservationsFilters = true;">Filters</button>
+					<div class="form-group col-sm-4">
+						<a class="btn btn-default btn-sm" @click="showReservationsFilters = true;">Filters</a>
 					</div>
 					<div class="col-xs-12">
 						<hr class="divider inv">
@@ -210,7 +218,7 @@
 											</div>
 											<div class="col-xs-3 text-right action-buttons">
 												<a class="btn btn-xs btn-primary-hollow" @click="addPassenger(reservation)">
-													<span class="fa fa-ellipsis-h"></span>
+													<span class="fa fa-plus"></span>
 												</a>
 												<a class="btn btn-xs btn-default-hollow" role="button" data-toggle="collapse" data-parent="#reservationsAccordion" :href="'#reservationItem' + $index" aria-expanded="true" aria-controls="collapseOne">
 													<i class="fa fa-angle-down"></i>
@@ -303,9 +311,10 @@
                     designation: '',
                     age: [0, 120],
 	                search: '',
+                    notInTransport: this.transport.id
                 },
 
-	            PassengersResource: this.$resource('transports{/transport}/passengers{/passenger}')
+	            PassengersResource: this.$resource('transports{/transport}/passengers{/passenger}', { transport: this.transport.id })
             }
         },
 	    watch: {
@@ -334,15 +343,30 @@
             },
 
             getPassengers() {
-                let params = _.extend({ include: 'reservation.trip.campaign,reservation.trip.group,reservation.companions' }, this.passengersFilters);
+                let params = _.extend({
+	                include: 'reservation.trip.campaign,reservation.trip.group,reservation.companions',
+                    page: this.passengersPagination.current_page,
+                }, this.passengersFilters);
 
-                this.PassengersResource.get({ transport: this.transport.id}, params)
+                this.PassengersResource.get(params).then(function (response) {
+                    this.passengers = response.body.data;
+                    this.passengersPagination = response.body.meta.pagination;
+                });
             },
 	        addPassenger(reservation) {
-
+                this.PassengersResource.save({
+                    transport_id: this.transport.id,
+                    reservation_id: reservation.id,
+                }).then(function (response) {
+                    this.getPassengers();
+                    this.searchReservations();
+                });
 	        },
 	        removePassenger(passenger) {
-
+                this.PassengersResource.delete({ passenger: passenger.id }).then(function (response) {
+                    this.getPassengers();
+                    this.searchReservations();
+                });
 	        },
             searchReservations(){
                 let params = {
