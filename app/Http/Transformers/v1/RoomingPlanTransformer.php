a8418183d046ce2dc1a17ff3bb603e92abe2a34e
Neil Keena <nkeena1@gmail.com>
2017-07-03 16:34:52 -0700

    update filters and transformers for multi-group support

--- app/Http/Transformers/v1/RoomingPlanTransformer.php
+++ app/Http/Transformers/v1/RoomingPlanTransformer.php
@@ -15,7 +15,7 @@ class RoomingPlanTransformer extends TransformerAbstract
      *
      * @var array
      */
-    protected $availableIncludes = ['rooms', 'group'];
+    protected $availableIncludes = ['rooms', 'groups'];
 
     public function transform(RoomingPlan $plan)
     {
@@ -63,11 +63,11 @@ class RoomingPlanTransformer extends TransformerAbstract
         return $this->collection($rooms, new RoomTransformer);
     }
 
-    public function includeGroup(RoomingPlan $plan)
+    public function includeGroups(RoomingPlan $plan)
     {
-        $group = $plan->group;
+        $group = $plan->groups;
 
-        return $this->item($group, new GroupTransformer);
+        return $this->collection($group, new GroupTransformer);
     }
 
     private function validateParams($params)
